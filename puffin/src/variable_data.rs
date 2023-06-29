761880c0a55c6774c179743126f832599848a5ae
nimics <micol.giacomin@gmail.com>
2023-06-29 14:28:56 +0200

    use typetag

--- puffin/src/variable_data.rs
+++ puffin/src/variable_data.rs
@@ -4,12 +4,16 @@
 use std::{
     any::{Any, TypeId},
     fmt::Debug,
+    hash::{Hash, Hasher},
 };
+
+use serde::{Deserialize, Serialize};
 
 // use libafl::inputs::HasBytesVec;
 
 // use crate::algebra::{atoms::Variable, Matcher};
 
+#[typetag::serde(tag = "type")]
 pub trait VariableData: Debug {
     fn boxed(&self) -> Box<dyn VariableData>;
     fn boxed_any(&self) -> Box<dyn Any>;
@@ -19,9 +23,11 @@ pub trait VariableData: Debug {
 
 /// A VariableData is cloneable and has a `'static` type. This data type is used throughout
 /// tlspuffin to handle data of dynamic size.
+
+#[typetag::serde]
 impl<T: 'static> VariableData for T
 where
-    T: Clone + Debug,
+    T: Debug + Serialize + Deserialize,
 {
     fn boxed(&self) -> Box<dyn VariableData> {
         Box::new(self.clone())
@@ -37,5 +43,37 @@ where
 
     fn type_name(&self) -> &'static str {
         std::any::type_name::<T>()
+    }
+}
+
+// implementations for box
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct GlobalVariableData(pub Box<dyn VariableData>);
+
+/* impl GlobalVariableData {
+    pub fn new(data: dyn VariableData) -> GlobalVariableData {
+        GlobalVariableData(Box::new(data))
+    }
+} */
+
+impl Clone for GlobalVariableData {
+    fn clone(&self) -> Self {
+        self.clone()
+    }
+}
+
+impl Hash for GlobalVariableData {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.hash(state)
+    }
+}
+impl GlobalVariableData {
+    pub fn as_ref(&self) -> &dyn VariableData {
+        self.0.as_ref()
+    }
+
+    pub fn type_name(&self) -> &'static str {
+        self.0.type_name()
     }
 }
